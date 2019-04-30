694367699cf8f22ea018d2dbda0fe052a17d590d
Gordon Woodhull <gordon@woodhull.com>
2019-04-30 16:32:53 -0400

    address @s-u feedback
    * if package & path, use getNamespaceInfo to seek the package path,
    in case it was loaded with lib.loc
    * if full path, check it against user lib first before .libPaths()
    * print a warning if the subdirectory is not supported
    * simpler username regex
    
    Currently does no check if the file exists or permissions are ok - it's
    just string manipulation against various library paths. We could add that
    in a later commit.

--- R/htmlwidgets.R
+++ R/htmlwidgets.R
@@ -295,25 +295,36 @@ print.shiny.tag <- function(x, ..., view = interactive()) {
 rcloudHTMLDependency <- function(dep) {
   file <- dep$src$file
   if(!is.null(file)) {
+    # htmltools dependencies can be either file='absolute' or package='foo',file='relative'
+    # we want to know:
+    # * package name
+    # * is this a user library, from the current user's .libPath() or from lib.loc?
+    # * top directory within package - shared.R currently allow htmlwidgets, www, and lib
+    # * rest of file path
     package <- dep$package
     if(!is.null(package)) {
-      lib <- where_in_path(find.package(package), .libPaths())
+      pkgpath <- getNamespaceInfo(package, 'path')
+      user <- is_user_lib(pkgpath)
       rel_path <- paste0(package, '/', file)
     } else {
-      lib <- where_in_path(file, .libPaths())
-      if (is.na(lib)) {
-        warning("Cannot find htmlwidgets dependency: ", file)
-        return(dep)
+      user <- is_user_lib(file)
+      if(!is.null(user)) {
+        rel_path <- path_inside(file, rcloud.home('library', user=user))
+      } else {
+        lib <- where_in_path(file, .libPaths())
+        if (is.na(lib)) {
+          warning("Cannot find htmlwidgets dependency: ", file)
+          return(dep)
+        }
+        rel_path <- path_inside(file, lib)
       }
-      rel_path <- path_inside(file, lib)
     }
     c_rel_path <- path_components(rel_path)
-    pkg <- c_rel_path[1]
+    package <- c_rel_path[1]
 
-    ## strip off pkg/www or pkg/htmlwidgets
-    pkgpath <- paste(tail(c_rel_path, -2), collapse = "/")
+    ## strip off package/www or package/htmlwidgets
+    filepath <- paste(tail(c_rel_path, -2), collapse = "/")
 
-    user <- is_user_lib(lib)
     parts <- NULL
     if (length(c_rel_path) < 2) {
       warning("Invalid htmlwidgets dependency path: ", file)
@@ -321,11 +332,13 @@ rcloudHTMLDependency <- function(dep) {
     } else if (c_rel_path[2] == "htmlwidgets") {
       parts <- c('/shared.R', '_htmlwidgets')
       if(!is.null(user)) parts = c(parts, user)
-      parts <- c(parts, pkg, pkgpath)
+      parts <- c(parts, package, filepath)
     } else if (c_rel_path[2] %in% c("www", "lib")) {
       parts <- c('/shared.R')
       if(!is.null(user)) parts = c(parts, user)
-      parts <- c(parts, pkg, pkgpath)
+      parts <- c(parts, package, filepath)
+    } else {
+      warning("Invalid package subdirectory: ", c_rel_path[2])
     }
     if(!is.null(parts))
       dep$src$href <- paste0(parts, collapse='/')
@@ -335,7 +348,7 @@ rcloudHTMLDependency <- function(dep) {
 }
 
 is_user_lib <- function(path) {
-    found <- gsub(rcloud.home('library', user='([a-z_][a-z0-9_]*)'), '\\1', path) 
+    found <- gsub(rcloud.home('library', user='([^/]*)'), '\\1', path) 
     if(found == path) NULL else found
 }
 
